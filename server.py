96378968fec5116eea2e9ba1608ecd7fc89fa537
MacewanCassidy <cassidym8@mymacewan.ca>
2023-11-28 09:32:25 -0700

    Added documentation for increased clarity

--- server.py
+++ server.py
@@ -28,13 +28,13 @@ def server_program():
         print('Error in server socket binding:', e)
         sys.exit(1)
 
-    # Loading data from credentials file
+    # Loading data from credentials file, these are our authorized users.
     file = open("credentials.json", 'r')
     x = file.read()
     creds = json.loads(x)
     file.close()
 
-    # Creating a list of users with inboxes.
+    # Creating a list of users with inboxes. Could be improved.
     users = []
     for (uname, password) in creds.items():
         user = User(uname, password)
@@ -42,7 +42,7 @@ def server_program():
 
     print('The server is ready to accept connections')
 
-    # The server can only have one connection in its queue waiting for acceptance
+    # The server can only have five connection in its queue waiting for acceptance
     server.listen(5)
 
     while True:
@@ -53,10 +53,10 @@ def server_program():
 
             # SERVICE START
 
-            # Server receives credentials for authorization (RECV 1)
+            # Server receives credentials for authentication (RECV 1)
             username, password = connection_socket.recv(100).decode().split("\n")  # Decrypt information
 
-            # Server sends result of authorization (SEND 2)
+            # Server sends result of authentication (SEND 2)
             if password == creds.get(username):
                 running = True
                 connection_socket.send('1'.encode())
@@ -78,22 +78,21 @@ def server_program():
                                        "\n\t4) Terminate the connection\n".encode())  # Encrypt this
 
                 # Receive operation menu selection (RECV 4)
-                selection = connection_socket.recv(2).decode()  # Decode this
+                selection = connection_socket.recv(2).decode()  # Decrypt this
 
                 match selection:
 
-                    # Create and send email.
+                    # Create and send email operation.
                     case '1':
 
-                        # Decrypt these.
-                        destinations = connection_socket.recv(100).decode()
+                        # (RECV 5a)
+                        destinations = connection_socket.recv(100).decode()  # Decrypt these two
                         email = connection_socket.recv(10000).decode()
 
+                        # Adds time to email string.
+                        email = add_time(email)
 
-
-                        # Need to add time recieved to email.
-
-                        # Convert to list
+                        # Convert to list, to iterate through destination users.
                         if ';' in destinations:
                             destinations = destinations.split(';')
                         else:
@@ -101,21 +100,21 @@ def server_program():
 
                         # THIS COULD BE IMPROVED BY USING A DICTIONARY TO SEARCH FOR USERS.
                         # However, execution speed  for this assignment is not high priority.
+                        # This finds the recipient from users and adds the email string to their inbox.
                         for recipient in destinations:
                             for user in users:
                                 if recipient == user.username:
                                     user.inbox.append(email)
 
                     # Display inbox contents.
+                    # Needs to be updated to display email header/stud, currently displays entire email string.
                     case '2':
                         loop = str(len(active_user.inbox))
                         connection_socket.send(loop.encode())
                         for email in active_user.inbox:
                             connection_socket.send(email.encode())
 
-                        pass
-
-                    # Read indexed email.
+                    # Read indexed email. Grab email string from inbox to display.
                     case '3':
 
                         pass
@@ -137,6 +136,7 @@ def server_program():
             sys.exit(1)
 
 
+# Class to organize username, password and inbox in one piece of data.
 class User:
     def __init__(self, username, password):
         self.username = username
